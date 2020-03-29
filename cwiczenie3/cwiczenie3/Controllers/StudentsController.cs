f938043cbc98baff005ddb697b09bf0bf2c41dbf
s16522 <jadczak.tom@gmail.com>
2020-03-29 18:02:34 +0200

    cwiczenie 3 apbd

---
+++ cwiczenie3/cwiczenie3/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using cwiczenie3.DAL;
+using cwiczenie3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cwiczenie3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent() {
+            return Ok("Aktualizacja dokonczona");
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent() {
+            return Ok("Usuwanie ukończone");
+        }
+        
+    }
+}
